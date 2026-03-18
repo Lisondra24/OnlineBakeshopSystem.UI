c1aabb6fe13df518ea8c5699e0df218199a7fcef
Daniel Dave Lisondra <danieldavelisondra09@gmail.com>
2026-03-18 18:46:25 +0800

    Add project files.

---
+++ OnlineBakeshopSystem/Model/LoginModel.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace OnlineBakeshopSystem.Model
+{
+    public class LoginModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ OnlineBakeshopSystem/Model/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineBakeshopSystem.Model
+{
+    public class ServiceResponse<T>
+    {
+        public int Status { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+        public string Token { get; set; }
+    }
+}
