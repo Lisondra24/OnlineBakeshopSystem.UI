11aad5a012039ad0683f9c6a7e12e7774a338790
Daniel Dave Lisondra <danieldavelisondra09@gmail.com>
2026-03-26 16:46:37 +0800

    new pages for orders and users

---
+++ OnlineBakeshopSystem/Model/UserModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace OnlineBakeshopSystem.Model
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string ContactNo { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
