de40f8415347eee25e0d9b93cce81468348a608a
Daniel Dave Lisondra <danieldavelisondra09@gmail.com>
2026-03-27 17:06:31 +0800

    combining the regular orders and custom orders in orders.razor

---
+++ OnlineBakeshopSystem/Model/CustomOrderModel.cs
@@ -0,0 +1,26 @@
+﻿namespace OnlineBakeshopSystem.Model
+{
+    public class CustomOrderModel
+    {
+        public int CustomOrderId { get; set; }
+        public int UserId { get; set; }
+        public string OrderType { get; set; }
+        public string Flavor { get; set; }
+        public string Size { get; set; }
+        public string ColorTheme { get; set; }
+        public string MessageOnCake { get; set; }
+        public int? NumberOfLayers { get; set; }
+        public string ReferenceImage { get; set; }
+        public string SpecialNotes { get; set; }
+        public DateTime? DeliveryDate { get; set; }
+        public string DeliveryTime { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string PaymentStatus { get; set; }
+        public string OrderStatus { get; set; }
+        public decimal? QuotedPrice { get; set; }
+        public DateTime DateOrdered { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string ContactNo { get; set; }
+    }
+}
